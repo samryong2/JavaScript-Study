6db98e39715d36d54db5fd2a5a73c5540f9be972
Dev-hanseo <85448618+Dev-hanseo@users.noreply.github.com>
2021-06-15 05:26:17 +0900

    browser 성능향상
    layout 변경에서 composition 변경으로 위치이동

--- browser/007.js
+++ browser/007.js
@@ -1,22 +1,31 @@
 const screenLog = document.querySelector('#screen-log');
-document.addEventListener('mousemove', logKey);
+
 
 const xLine = document.querySelector(".x-line");
 const yLine = document.querySelector(".y-line");
-const target = document.querySelector(".target");
+const target = document.querySelector("img");
 
 
-function logKey(e) {
-    screenLog.innerText = `
-    Screen X/Y: ${e.screenX}, ${e.screenY}
-    Client X/Y: ${e.clientX}, ${e.clientY}`;
 
-    screenLog.style.left = `${e.clientX}px`;
-    screenLog.style.top = `${e.clientY}px`;
+addEventListener('load',()=>{
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfHeigt = targetRect.height/2;
+    const targetHalfWidth = targetRect.width/2;
 
-    yLine.style.left = `${e.clientX}px`;
-    target.style.left = `${e.clientX}px`;
-    xLine.style.top = `${e.clientY}px`;
-    target.style.top = `${e.clientY}px`;
+    console.log(targetHalfHeigt);
+    console.log(targetHalfWidth);
 
-}
+    document.addEventListener('mousemove', (e)=>{
+        screenLog.innerText = 
+        `${e.clientX}px, ${e.clientY}px`;
+    
+        yLine.style.transform = `translateX(${e.clientX}px)`;
+        xLine.style.transform = `translateY(${e.clientY}px)`;
+        target.style.transform = 
+        `translate(${e.clientX}px, ${e.clientY})`;
+        
+        screenLog.style.transform = `translate(${e.clientX+20}px, ${e.clientY+20}px)`;
+    });
+})
+
+
