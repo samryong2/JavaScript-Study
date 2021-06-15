5dadd01b350980e0188e81f78a6d340db2849c0c
Dev-hanseo <85448618+Dev-hanseo@users.noreply.github.com>
2021-06-16 05:25:19 +0900

    간단한 쇼핑 리스트
    간단한 쇼핑 리스트 만들기

---
+++ shopping-list/shippingList.js
@@ -0,0 +1,65 @@
+`use strict`;
+
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
+const items = document.querySelector('.items');
+
+
+function onAdd() {
+    // 1. 사용자가 입력한 텍스트를 받아옴
+    const text = input.value;
+
+    if (text === '') {
+        input.focus();
+        return;
+    }
+
+    // 2. 새로운 아이템을 만듬 (텍스트 + 삭제 버튼)
+    const item = createItem(text);
+    // 3. items 컨테이너안에 새로 만든 아이템을 추가한다
+    items.appendChild(item);
+    // 4. 인풋을 초기화 한다.
+    input.nodeValue = '';
+    input.focus();
+}
+
+function createItem(text) {
+    const itemRow = document.createElement("li");
+    itemRow.setAttribute('class', 'item__row');
+
+    const item = document.createElement('div');
+    item.setAttribute('class','item');
+
+    const span = document.createElement('span');
+    span.setAttribute('class', 'item__name');
+    span.innerText = text;
+
+    const delteBtn = document.createElement('button');
+    delteBtn.setAttribute('class', 'item__delete');
+    delteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    delteBtn.addEventListener('click',()=>{
+        items.removeChild(itemRow);
+    })
+    
+    const itemDivider = document.createElement('div');
+    itemDivider.setAttribute('class', 'item__divider');
+
+    item.appendChild(span);
+    item.appendChild(delteBtn);
+
+    itemRow.appendChild(item);
+    itemRow.appendChild(itemDivider);
+
+    return itemRow;
+}
+
+addBtn.addEventListener("click",()=>{
+    onAdd();
+})
+
+input.addEventListener('keypress',(event)=>{
+    if (event.keyCode === 13) {
+        onAdd();
+        input.value = "";
+    }
+})
